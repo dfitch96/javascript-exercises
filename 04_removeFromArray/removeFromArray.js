bdb9cdec117350387a52eecd3d2870754554c381
Dylan Fitch <kkarl30@gmail.com>
2024-11-30 23:06:04 -0500

    remove elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    
+    let newArray = [];
+
+    for(const element of arr){
+        if (!args.includes(element)){
+            newArray.push(element);
+        }
+    }
+    return newArray;
+    
+
 };
 
 // Do not edit below this line
