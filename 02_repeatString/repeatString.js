93adc572488c3d73c120248d5b71bc7aa96374a1
Dylan Fitch <kkarl30@gmail.com>
2024-11-30 22:09:16 -0500

    finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
 
+    if (num < 0){
+        return "ERROR";
+    }
+
+    res = '';
+    for(let i = 0; i < num; i++){
+        res += text;
+    }
+
+    return res;
 };
 
 // Do not edit below this line
