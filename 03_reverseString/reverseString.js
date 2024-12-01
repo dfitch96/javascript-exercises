bdd2c01ac4500f05decd343d34822ba71b9a9e79
Dylan Fitch <kkarl30@gmail.com>
2024-11-30 22:14:44 -0500

    reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+
+    reversed = '';
+
+    for(let i = text.length - 1; i >= 0; i--){
+        reversed += text[i];
+    }
+
+    return reversed;
 
 };
 
