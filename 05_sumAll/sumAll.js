ac987c377b93b547b726e2a4ced06ac0fd83c89c
Dylan Fitch <kkarl30@gmail.com>
2024-11-30 23:16:49 -0500

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+
+    i = num1 < num2 ? num1 : num2;
+    max = num1 < num2 ? num2 : num1;
+    
+    sum = 0;
+    for(;i <= max ;i++){
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
