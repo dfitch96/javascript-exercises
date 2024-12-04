b4b322efb067aef9c5ecbc691e2d1c37a3f0fea6
Dylan Fitch <kkarl30@gmail.com>
2024-12-04 16:57:38 -0500

    exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (text) {
+
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanedText = text
+    .toLowerCase()
+    .split('')
+    .filter(ch => alphanumerical.includes(ch))
+    .join('');
+
+    const reversedText = cleanedText.split('').reverse().join('');
+
+    return cleanedText === reversedText;
 
 };
+
+palindromes('Racecar!');
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,33 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+
+    let count = 0;
+    if(typeof count !== 'number'){
+        count = parseInt(countArg);
+    } else{
+        count = countArg;
+    }
+
+    if(count == 0){
+        return 0;
+    } 
+    if(count <= 0){
+        return "OOPS";
+    }
+
+    let first = 1;
+    let second = 1;
+
+
+    for(let i = 2; i < count; i++){
+        let temp = first + second;
+        first = second;
+        second = temp;
+    }
+
+    return second;
+
+
+
 
 };
 
