8ada35a6d21f87d2f0af59da1fd1ceac457ca685
Dylan Fitch <kkarl30@gmail.com>
2024-12-04 17:12:28 -0500

    exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const orderedPeople = people.sort((a, b) =>{
+        if (!a.yearOfDeath){
+            a.yearOfDeath = (new Date()).getFullYear();
+        } else if (!b.yearOfDeath){
+            b.yearOfDeath = (new Date()).getFullYear();
+        }
 
+        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth)
+
+    });
+    return orderedPeople.pop();
 };
 
 // Do not edit below this line
