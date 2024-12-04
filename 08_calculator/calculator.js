2e4c2c32bae0148663d6badca880e64d2b054090
Dylan Fitch <kkarl30@gmail.com>
2024-12-04 16:31:03 -0500

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a=0, b=0) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a=0, b=0) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+    return nums.reduce((sum, curr) => sum + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((product, curr) => product * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product = 1;
+  for(let i = n; i > 1; i--){
+    product *= i;
+  }
+
+  return product;
+
+
 };
 
 // Do not edit below this line
